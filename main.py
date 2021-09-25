fb888b3c7ed9f03589155a36951bd0c2994c3d51
venketesh <venketeshofficial.1@gmail.com>
2021-09-24 18:12:55 -0700

    Flask Backend server app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingera'
@@ -13,7 +14,7 @@ class Contacts(db.Model):
     name = db.Column(db.String(80), nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
     msg = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
 
@@ -36,7 +37,15 @@ def post():
 def contact():
     if (request.method == 'POST'):
         '''Add Entry to database '''
-    request.form.get('')
+        name =request.form.get('name')
+        email =request.form.get('email')
+        phone =request.form.get('phone')
+        message =request.form.get('message')
+
+        entry = Contacts(name=name, phone_num=phone, msg=message, email=email, date=datetime)
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 
